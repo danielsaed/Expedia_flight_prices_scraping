6cb739dd1f8844e13cae5601fa3b3c14b93616cc
Saed <84155356+danielsaed@users.noreply.github.com>
2024-12-31 21:41:59 -0700

    Change page lino and disable unusable xpaths

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -8,6 +8,7 @@ import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
 import os
 import time
+import undetected_chromedriver as uc
 
 #display = Display(visible=0, size=(800, 800))  
 #display.start()
@@ -18,23 +19,27 @@ chromedriver_autoinstaller.install()  # Check if the current version of chromedr
 chrome_options = webdriver.ChromeOptions()  
 
 options = [
-  # Define window size here
-    #"--headless",
-   "--window-size=1200,1200",
-   "--ignore-certificate-errors"
+    "--user-data-dir=C:/Users/saedi/AppData/Local/Google/Chrome/User Data",
+    "--profile-directory=Default",
+    "--window-size=1200,1200",
+    "--ignore-certificate-errors",
+    "--lang=es",
+    "--disable-dev-shm-usage",  # Overcome limited resource problems
+    "--no-sandbox",  # Bypass OS security model
+    "--disable-gpu",  # Applicable to windows os only
+    "--disable-extensions",  # Disable extensions
+    "--remote-debugging-port=9222"  # Enable remote debugging
 ]
 
 for option in options:
     chrome_options.add_argument(option)
 
-
-    
 driver = webdriver.Chrome(options = chrome_options)
 
-driver.get('https://www.expedia.mx/')
+driver.get(r'https://www.expedia.mx/Flights-Search?flight-type=on&mode=search&trip=oneway&leg1=from:Tepic,%20Nayarit,%20M%C3%A9xico,to:Ciudad%20de%20M%C3%A9xico,%20M%C3%A9xico%20(MEX-Aeropuerto%20Internacional%20de%20la%20Ciudad%20de%20M%C3%A9xico),departure:14/01/2025TANYT,fromType:CITY,toType:AIRPORT&options=cabinclass:economy&fromDate=14/01/2025&d1=2025-1-14&passengers=adults:1,infantinlap:N')
 print('done')
-vuelos = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="multi-product-search-form-1"]/div/div/div/div/div[1]/ul/li[2]/a/span'))).click()
-time.sleep(5)
+'''vuelos = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="multi-product-search-form-1"]/div/div/div/div/div[1]/ul/li[2]/a/span'))).click()
+time.sleep(30)
 
 viaje_sencillo = driver.find_element(By.XPATH,"//span[normalize-space()='Viaje sencillo']").click()
 time.sleep(.5)
@@ -46,6 +51,15 @@ select_tepic = WebDriverWait(driver, 10).until(EC.presence_of_element_located((B
 time.sleep(.5)
 destino = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"//button[@aria-label='Destino']"))).click()
 time.sleep(.5)
+destino_select = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"//input[@id='destination_select']"))).send_keys("Ciudad de México")
+time.sleep(.5)
+destino_select = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"//button[@aria-label='Ciudad de México (MEX - Aeropuerto Internacional de la Ciudad de México) México']"))).click()
+time.sleep(.5)
+destino_select = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"//input[@id='destination_select']"))).click()
+time.sleep(.5)
+destino_select = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"//input[@id='destination_select']"))).click()
+time.sleep(.5)
+input()'''
 
 #xpath viaje sencillo //span[normalize-space()='Viaje sencillo']
 #xpath boton origen //button[@aria-label='Origen']
