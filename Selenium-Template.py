efaa8f3f84b39aabe6775adc8515370cc343aded
Saed <84155356+danielsaed@users.noreply.github.com>
2025-01-05 21:37:08 -0700

    Separete while usexpath, added comment local chrome driver

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -19,13 +19,19 @@ def use_xpath(xpath,time):
 def random_delay(start=1, end=3):
     time.sleep(random.uniform(start, end))
 
-display = Display(visible=0, size=(800, 800))  
-display.start()
+#display = Display(visible=0, size=(800, 800))  
+#display.start()
 
-chromedriver_autoinstaller.install()# Check if the current version of chromedriver exists
-                                    # and if it doesn't exist, download it automatically,
+dict_ = {}
+quantity_flights= 0
+
+
+
+chromedriver_autoinstaller.install()# Check if the current version of chromedriver exists and if it doesn't exist, download it automatically,
 ua = UserAgent()
 user_agent = ua.random
+
+
 
 chrome_options = uc.ChromeOptions()
 chrome_options.add_argument(f"user-agent={user_agent}")
@@ -38,14 +44,12 @@ options = [
     "--disable-dev-shm-usage",  # Overcome limited resource problems
     "--no-sandbox"]  # Bypass OS security model
 
-dict_ = {}
-quantity_flights= 0
+for option in options:
+    chrome_options.add_argument(option)
 
-dict_[str(quantity_flights + 1)] = {
-        1:0
-    }
+'''chromedriver_path = r"C:/path/to/your/chromedriver.exe"
+driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)'''
 
-print(dict_)
 
 driver = uc.Chrome(options=chrome_options)
 driver.get(r'https://www.expedia.mx/Flights-Search?flight-type=on&mode=search&trip=oneway&leg1=from:Tepic,%20Nayarit,%20M%C3%A9xico,to:Ciudad%20de%20M%C3%A9xico,%20M%C3%A9xico%20(MEX-Aeropuerto%20Internacional%20de%20la%20Ciudad%20de%20M%C3%A9xico),departure:14/01/2025TANYT,fromType:CITY,toType:AIRPORT&options=cabinclass:economy&fromDate=14/01/2025&d1=2025-1-14&passengers=adults:1,infantinlap:N')
@@ -54,12 +58,18 @@ use_xpath("//li[@data-test-id='offer-listing'][1]//div/button/span",180)
 
 try:
     while True:
+        print(f"//li[@data-test-id='offer-listing'][{quantity_flights+1}]/div/div/div/div/div[1]/div[2]/div/div/section/span")
+
+        price = use_xpath(f"//li[@data-test-id='offer-listing'][{quantity_flights+1}]/div/div/div/div/div[1]/div[2]/div/div/section/span", 120)
+        tiempo = use_xpath(f"//li[@data-test-id='offer-listing'][{quantity_flights+1}]/div/div/div/div/div[1]/div[1]/div/div[2]/div", 120)
+        aerolinea = use_xpath(f"//li[@data-test-id='offer-listing'][{quantity_flights+1}]/div/div/div/div/div[1]/div[1]/div/div[3]/div[2]", 120)
+        horario = use_xpath(f"//li[@data-test-id='offer-listing'][{quantity_flights+1}]/div/div/div/div/div[1]/div[1]/div/div[1]/div[2]", 120)
 
         dict_[str(quantity_flights + 1)] = {
-            "price": use_xpath(f"//li[@data-test-id='offer-listing'][{quantity_flights+1}]/div/div/div/div/div[1]/div[2]/div/div/section/span",random_delay(1,2)),
-            "tiempo":use_xpath(f"//li[@data-test-id='offer-listing'][{quantity_flights+1}]/div/div/div/div/div[1]/div[1]/div/div[2]/div",random_delay(1,2)),
-            "aerolinea": use_xpath(f"//li[@data-test-id='offer-listing'][{quantity_flights+1}]/div/div/div/div/div[1]/div[1]/div/div[3]/div[2]",random_delay(1,2)),
-            "horario":use_xpath(f"//li[@data-test-id='offer-listing'][{quantity_flights+1}]/div/div/div/div/div[1]/div[1]/div/div[1]/div[2]",random_delay(1,2))
+            "price": price.text,
+            "tiempo": tiempo.text,
+            "aerolinea": aerolinea.text,
+            "horario": horario.text
         }
 
         quantity_flights += 1
@@ -68,6 +78,8 @@ except Exception as e:
     print(f"Se encontraron {quantity_flights} vuelos")
     print("An error occurred:")
     traceback.print_exc()
+    tb = traceback.extract_tb(e.__traceback__)
+    print(f"Error occurred in line: {tb[-1].lineno}")
 
 print(dict_)
 
