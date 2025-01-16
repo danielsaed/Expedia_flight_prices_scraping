6d7e7d5f51a344cb24f3b13b72b9e875230e3ff5
Saed <danielsaed99@hotmail.com>
2025-01-16 02:16:59 -0600

    fixed column name

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -105,7 +105,7 @@ for i in range(len(dates)):
 
 dict_ = {}
 
-df = pd.DataFrame(columns=["Price", "Flight Time","Stop over","Stop over place","Airline", "Departure time","Date of flight","Destination place"])
+df = pd.DataFrame(columns=["Price", "Flight time","Stop over","Stop over place","Airline", "Departure time","Date of flight","Destination place"])
 
 
 chromedriver_autoinstaller.install()# Check if the current version of chromedriver exists and if it doesn't exist, download it automatically,
