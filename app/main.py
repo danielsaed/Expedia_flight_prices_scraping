a2830fdc9556c8ae7cfa1062289efeecb225725f
Saed <danielsaed99@hotmail.com>
2025-01-30 12:30:55 -0600

    erased input city requirements

--- app/main.py
+++ app/main.py
@@ -12,26 +12,7 @@ import re
 from functions import *
 
 
-def use_xpath(xpath,time):
-    return WebDriverWait(driver, time).until(EC.presence_of_element_located((By.XPATH, xpath)))
 
-
-def get_second_parentheses_content(text):
-    """
-    Gets the content inside the second pair of parentheses from a string.
-
-    Args:
-        text (str): The input string.
-
-    Returns:
-        str: The content inside the second pair of parentheses, or an empty string if not found.
-    """
-    # Use regular expression to find all content inside parentheses
-    matches = re.findall(r'\(.*?\)', text)
-    # Return the content inside the second pair of parentheses if it exists
-    first_content = matches[0] if len(matches) >= 1 else ""
-    second_content = matches[1] if len(matches) >= 2 else ""
-    return first_content, second_content
 #-----------------INPUT------------------#
 '''
 Input the flight data with the links of the flight with the next format:
@@ -41,7 +22,7 @@ Input the flight data with the links of the flight with the next format:
 dic_input_flight_links = {
     #1:['Tepic','Ciudad de México','https://www.expedia.mx/Flights-Search?flight-type=on&mode=search&trip=oneway&leg1=from:Tepic,%20Nayarit,%20M%C3%A9xico,to:Ciudad%20de%20M%C3%A9xico,%20M%C3%A9xico%20(MEX-Aeropuerto%20Internacional%20de%20la%20Ciudad%20de%20M%C3%A9xico),departure:14/01/2025TANYT,fromType:CITY,toType:AIRPORT&options=cabinclass:economy&fromDate=14/01/2025&d1=2025-1-14&passengers=adults:1,infantinlap:N'],
     #2:['Ciudad de México','Tepic','https://www.expedia.mx/Flights-Search?flight-type=on&mode=search&trip=oneway&leg1=from:Recinto%20Arena%20Ciudad%20de%20M%C3%A9xico,%20Ciudad%20de%20M%C3%A9xico,%20M%C3%A9xico,to:Tepic,%20Nayarit,%20M%C3%A9xico,departure:29/01/2025TANYT,fromType:POI,toType:CITY&options=cabinclass:economy&fromDate=29/01/2025&d1=2025-1-29&passengers=adults:1,infantinlap:N'],
-    3:['','','https://www.expedia.mx/Flights-Search?flight-type=on&mode=search&trip=oneway&leg1=from:Ciudad%20de%20M%C3%A9xico,%20M%C3%A9xico%20(MEX-Aeropuerto%20Internacional%20de%20la%20Ciudad%20de%20M%C3%A9xico),to:Tokio,%20Jap%C3%B3n%20(TYO-Todos%20los%20aeropuertos),departure:12/02/2025TANYT,fromType:AIRPORT,toType:METROCODE&options=cabinclass:economy&fromDate=12/02/2025&d1=2025-2-12&passengers=adults:1,infantinlap:N']
+    3:'https://www.expedia.mx/Flights-Search?flight-type=on&mode=search&trip=oneway&leg1=from:Ciudad%20de%20M%C3%A9xico,%20M%C3%A9xico%20(MEX-Aeropuerto%20Internacional%20de%20la%20Ciudad%20de%20M%C3%A9xico),to:Tokio,%20Jap%C3%B3n%20(TYO-Todos%20los%20aeropuertos),departure:12/02/2025TANYT,fromType:AIRPORT,toType:METROCODE&options=cabinclass:economy&fromDate=12/02/2025&d1=2025-2-12&passengers=adults:1,infantinlap:N'
 }
 
 #input the months that you want to search, can be in any format, numbers, short name or long name
@@ -94,7 +75,7 @@ for option in options:
 #generate the links, dates, the origin and arrival places
 for i in range(len(dates)):
     for key in dic_input_flight_links:
-        links.append([generate_dynamic_url(dic_input_flight_links[key][2],dates[i]),dates[i],dic_input_flight_links[key][0],dic_input_flight_links[key][1]])
+        links.append([generate_dynamic_url(dic_input_flight_links[key][2],dates[i]),dates[i]])
 #----------------------------------------#
 
 
@@ -106,7 +87,7 @@ try:
     driver = uc.Chrome(options=chrome_options)
     driver.get(links[0][0])
 
-    for current_link,date,origin,arrival in links:
+    for current_link,date in links:
         
         quantity_flights=0
         
@@ -130,7 +111,7 @@ try:
         driver.switch_to.window(driver.window_handles[-1])
         print("Switched back to new tab")
 
-        use_xpath("//li[@data-test-id='offer-listing']",180)
+        use_xpath(driver,"//li[@data-test-id='offer-listing']",180)
         time.sleep(5)
         #elements = driver.find_elements(By.XPATH, "//div[@data-test-id='price-column']")
         elements = driver.find_elements(By.XPATH, "//li[@data-test-id='offer-listing']")
@@ -151,8 +132,8 @@ try:
                     "Airline": "",
                     "Departure time": "",
                     "Date of flight":date,
-                    "Destination place":arrival,
-                    "Origin place":origin,
+                    "Destination place":"",
+                    "Origin place":"",
                     "Airline 1":"",
                     "Airline 2":"",
                     "Airline 3":""
@@ -167,21 +148,21 @@ try:
 
                 #-----------------SCRAPPING DATA------------------#
                 try:
-                    origin = use_xpath(f"//div[@data-test-id='typeahead-originInput']//button", 60)
+                    origin = use_xpath(driver,f"//div[@data-test-id='typeahead-originInput']//button", 60)
                     dict_[str(quantity_flights + 1)]["Origin place"] = re.sub(r'\(.*?\)', '', origin.text).strip()
                 except:
                     print("No se encontro el tiempo")
                     dict_[str(quantity_flights + 1)]["Origin place"] ="Origin place not found"
                 
                 try:
-                    arrival = use_xpath(f"//div[@data-test-id='typeahead-destinationInput']//button", 60)
+                    arrival = use_xpath(driver,f"//div[@data-test-id='typeahead-destinationInput']//button", 60)
                     dict_[str(quantity_flights + 1)]["Destination place"] = re.sub(r'\(.*?\)', '', arrival.text).strip()
                 except:
                     print("No se encontro el tiempo")
                     dict_[str(quantity_flights + 1)]["Destination place"] = "Destination place not found"
 
                 try:
-                    places = use_xpath(f"(//div[@data-stid='secondary-section'])[{quantity_flights+1}]/div[2]/div", 5)
+                    places = use_xpath(driver,f"(//div[@data-stid='secondary-section'])[{quantity_flights+1}]/div[2]/div", 5)
                     first,second = get_second_parentheses_content(places.text)
                     dict_[str(quantity_flights + 1)]["Origin place"] = dict_[str(quantity_flights + 1)]["Origin place"] + " " + first
                     dict_[str(quantity_flights + 1)]["Destination place"] = dict_[str(quantity_flights + 1)]["Destination place"] + " " + second
@@ -189,12 +170,12 @@ try:
                     print("No se encontro parentesis")
 
                 try:
-                    price = use_xpath(f"(//div[@data-test-id='price-column'])[{quantity_flights+1}]/div/section/span[2]", 3)#data-stid
+                    price = use_xpath(driver,f"(//div[@data-test-id='price-column'])[{quantity_flights+1}]/div/section/span[2]", 3)#data-stid
                     dict_[str(quantity_flights + 1)]["Price"] = re.sub(r'\D', '', price.text)
                     
                 except:
                     try:
-                        price = use_xpath(f"(//div[@data-stid='price-column'])[{quantity_flights+1}]/div/section/span[2]", 5)#data-stid
+                        price = use_xpath(driver,f"(//div[@data-stid='price-column'])[{quantity_flights+1}]/div/section/span[2]", 5)#data-stid
                         dict_[str(quantity_flights + 1)]["Price"] = re.sub(r'\D', '', price.text)
                     except:
                         print("No se encontro el Price")
@@ -202,13 +183,13 @@ try:
                         dict_[str(quantity_flights + 1)]["Price"] = "Price not found"
 
                 try:
-                    flight_time = use_xpath(f"(//div[@data-stid='tertiary-section'])[{quantity_flights+1}]/div/span[1]", 5)
+                    flight_time = use_xpath(driver,f"(//div[@data-stid='tertiary-section'])[{quantity_flights+1}]/div/span[1]", 5)
                     dict_[str(quantity_flights + 1)]["Flight time"] = flight_time.text
                 except:
                     print("No se encontro el tiempo")
                     dict_[str(quantity_flights + 1)]["Flight time"] = "Flight time not found"
                 try:
-                    stop_over = use_xpath(f"(//div[@data-stid='tertiary-section'])[{quantity_flights+1}]/div/span[3]", 5)
+                    stop_over = use_xpath(driver,f"(//div[@data-stid='tertiary-section'])[{quantity_flights+1}]/div/span[3]", 5)
                     if stop_over.text == "1 escala":
                         dict_[str(quantity_flights + 1)]["Stop over"] = 1
                     elif stop_over.text == "2 escalas":
@@ -221,20 +202,20 @@ try:
                 try:
 
                     if dict_[str(quantity_flights + 1)]["Stop over"] == "2 escalas":
-                        stop_over_place = use_xpath(f"(//div[@data-stid='tertiary-section'])[{quantity_flights+1}]/div[2]/span[1]", 1)
+                        stop_over_place = use_xpath(driver,f"(//div[@data-stid='tertiary-section'])[{quantity_flights+1}]/div[2]/span[1]", 1)
                         dict_[str(quantity_flights + 1)]["Stop over place"] = stop_over_place.text
-                        stop_over_place = use_xpath(f"(//div[@data-stid='tertiary-section'])[{quantity_flights+1}]/div[2]/span[3]", 1)
+                        stop_over_place = use_xpath(driver,f"(//div[@data-stid='tertiary-section'])[{quantity_flights+1}]/div[2]/span[3]", 1)
                         dict_[str(quantity_flights + 1)]["Stop over place"] = dict_[str(quantity_flights + 1)]["Stop over place"] +" and "+ stop_over_place.text
                     else:
 
-                        stop_over_place = use_xpath(f"(//div[@data-stid='tertiary-section'])[{quantity_flights+1}]/div[2]/div", 1)
+                        stop_over_place = use_xpath(driver,f"(//div[@data-stid='tertiary-section'])[{quantity_flights+1}]/div[2]/div", 1)
                         dict_[str(quantity_flights + 1)]["Stop over place"] = stop_over_place.text
                 except:
                     print("Stop over place not found")
                     dict_[str(quantity_flights + 1)]["Stop over place"] = "Stop over place not found"
                     
                 try:
-                    airline = use_xpath(f"(//div[@data-stid='secondary-section'])[{quantity_flights+1}]/div[3]/div", 5)
+                    airline = use_xpath(driver,f"(//div[@data-stid='secondary-section'])[{quantity_flights+1}]/div[3]/div", 5)
                     dict_[str(quantity_flights + 1)]["Airline"] = airline.text
                 except:
                     print("No se encontro Airline")
@@ -242,7 +223,7 @@ try:
                     #aerolinea = use_xpath(f"//li[@data-test-id='offer-listing'][{quantity_flights+1}]/div/div/div/div/div[1]/div[1]/div/div/div[3]/div", 5)
 
                 try:
-                    departure_time = use_xpath(f"(//div[@data-stid='secondary-section'])[{quantity_flights+1}]/div[1]/div/div/div[1]/div[1]/div", 120)
+                    departure_time = use_xpath(driver,f"(//div[@data-stid='secondary-section'])[{quantity_flights+1}]/div[1]/div/div/div[1]/div[1]/div", 120)
                     dict_[str(quantity_flights + 1)]["Departure time"] = departure_time.text
                 except:
                     print("No se encontro horario")
@@ -251,25 +232,25 @@ try:
                 try:
                     if "ltiples" in dict_[str(quantity_flights + 1)]["Airline"]:
                         print("Aerolineas multiples")
-                        use_xpath(f"//li[@data-test-id='offer-listing'][{quantity_flights+1}]", 60).click()
+                        use_xpath(driver,f"//li[@data-test-id='offer-listing'][{quantity_flights+1}]", 60).click()
                         time.sleep(1)
-                        use_xpath(f"//span[@class='uitk-expando-title']", 60).click()
+                        use_xpath(driver,f"//span[@class='uitk-expando-title']", 60).click()
                         time.sleep(1)
 
                         if stop_over.text == "1 escala":
-                            airline_1 = use_xpath(f"//div[@class='uitk-spacing']/div[1]/div[2]/span[2]", 1)
+                            airline_1 = use_xpath(driver,f"//div[@class='uitk-spacing']/div[1]/div[2]/span[2]", 1)
                             dict_[str(quantity_flights + 1)]["Airline 1"] = airline_1.text
-                            airline_2 = use_xpath(f"//div[@class='uitk-spacing']/div[2]/div[2]/span[2]", 1)
+                            airline_2 = use_xpath(driver,f"//div[@class='uitk-spacing']/div[2]/div[2]/span[2]", 1)
                             dict_[str(quantity_flights + 1)]["Airline 2"] = airline_2.text
                         
                         if stop_over.text == "2 escalas":
-                            airline_1 = use_xpath(f"//div[@class='uitk-spacing']/div[1]/div[2]/span[2]", 1)
+                            airline_1 = use_xpath(driver,f"//div[@class='uitk-spacing']/div[1]/div[2]/span[2]", 1)
                             dict_[str(quantity_flights + 1)]["Airline 1"] = airline_1.text
-                            airline_2 = use_xpath(f"//div[@class='uitk-spacing']/div[2]/div[2]/span[2]", 1)
+                            airline_2 = use_xpath(driver,f"//div[@class='uitk-spacing']/div[2]/div[2]/span[2]", 1)
                             dict_[str(quantity_flights + 1)]["Airline 2"] = airline_2.text
-                            airline_3 = use_xpath(f"//div[@class='uitk-spacing']/div[3]/div[2]/span[2]", 1)
+                            airline_3 = use_xpath(driver,f"//div[@class='uitk-spacing']/div[3]/div[2]/span[2]", 1)
                             dict_[str(quantity_flights + 1)]["Airline 3"] = airline_3.text
-                        use_xpath(f"(//*[name()='svg'][@aria-label='Cerrar y volver'])[1]", 60).click()
+                        use_xpath(driver,f"(//*[name()='svg'][@aria-label='Cerrar y volver'])[1]", 60).click()
 
             
                 except Exception as e:
